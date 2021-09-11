de76338c987bf180304585cd4dae1b49c7df6f80
Wintyswilly <Wintyboss@outlook.com>
2021-09-12 01:17:45 +0200

    hooked up first same to button2

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace RadioSampler
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Sampler());
         }
     }
 }
--- Sampler.cs
+++ Sampler.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
-
+using System.Media;
 namespace RadioSampler
 {
     public partial class Sampler : Form
@@ -13,6 +15,12 @@ namespace RadioSampler
         public Sampler()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SoundPlayer splayer = new SoundPlayer(@"D:\OneDrive\RadioSampler\RadioSampler\Jingle1.wav");
+            splayer.Play();
         }
     }
 }
