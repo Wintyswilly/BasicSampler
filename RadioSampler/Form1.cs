d768b179c04b8dfd41b9bcba5573abe003476918
Davey Boyle <davey@elucidan.com>
2021-10-01 16:19:29 +0100

    Refactored sampler functionality, added dependency injection and added ability to stop play

--- RadioSampler/Form1.cs
+++ RadioSampler/Form1.cs
@@ -1,390 +1,190 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Media;
-namespace RadioSampler
+﻿namespace RadioSampler
 {
+    using ApplicationServices.Sampler;
+    using Autofac;
+    using System;
+    using System.Windows.Forms;
+
     public partial class IncapableSampler : Form
     {
 
+        private static IContainer Container { get; set; }
 
         public IncapableSampler()
         {
             InitializeComponent();
+            var builder = new ContainerBuilder();
+            builder.RegisterInstance(new SamplerControls()).As<ISamplerControls>();
+            Container = builder.Build();
+        }
+      
+        private void ProcessButtonClick(ListBox listBox)
+        {
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var player = scope.Resolve<ISamplerControls>();
+                player.StopAnySoundPlaying();
 
+
+                if (listBox.Items.Count is 0)
+                {
+                    MessageBox.Show("No File Clown Shoes!");
+                }
+                else
+                {
+                    if (listBox.SelectedItems.Count is 0)
+                    {
+                        MessageBox.Show("Stop being mean to me! select a file");
+                    }
+                    else
+                    {
+                        string curItem = listBox.SelectedItem.ToString();
+                        int index = listBox.FindString(curItem);
+                        listBox.SetSelected(index, true);
+                        object fileLocationobj = curItem;
+                        string fileLocation = fileLocationobj.ToString();
+
+                        if (listBox.SelectedItem.ToString().EndsWith(".mp3"))
+                        {
+                            player.PlayWindowsMedia(fileLocation);
+                        }
+                        else
+                        {
+                            player.PlaySoundPlayer(fileLocation);
+                        }
+                    }
+                }
+            }
         }
 
         public void Button1_Click(object sender, EventArgs e)
         {
-
-
-            if (listBox1.Items.Count is 0) 
-            { System.Windows.Forms.MessageBox.Show("No File Clown Shoes!"); }
-            else
-            {
-                if (listBox1.SelectedItems.Count is 0)
-                { System.Windows.Forms.MessageBox.Show("Stop being mean to me! select a file"); }
-                else
-                {
-                if(listBox1.SelectedItem.ToString().EndsWith(".mp3"))
-                    {
-
-                        
-                        string curItem = listBox1.SelectedItem.ToString();
-                        int index = listBox1.FindString(curItem);
-                        listBox1.SetSelected(index, true);
-                        object fileLocationobj = curItem;
-                        string fileLocation = fileLocationobj.ToString();
-                        // play mp3
-                        WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer
-                        {
-                            URL = fileLocation
-                        };
-                        wplayer.controls.play();
-
-                    }
-                    else {
-                        string curItem = listBox1.SelectedItem.ToString();
-                        int index = listBox1.FindString(curItem);
-                        listBox1.SetSelected(index, true);
-                        object fileLocationobj = curItem;
-                        string fileLocation = fileLocationobj.ToString();
-                       // play wav
-                        SoundPlayer splayer = new SoundPlayer(soundLocation: fileLocation);
-                        splayer.Play();
-                    }
-                }
-            }
+            ProcessButtonClick(listBox1);
         }
 
         private void Button2_Click(object sender, EventArgs e)
         {
-            if (listBox2.Items.Count is 0)
-            { System.Windows.Forms.MessageBox.Show("No File Clown Shoes!"); }
-            else
-            {
-                if (listBox2.SelectedItems.Count is 0)
-                { System.Windows.Forms.MessageBox.Show("Stop being mean to me! select a file"); }
-                else
-                {
-
-                    if (listBox2.SelectedItem.ToString().EndsWith(".mp3"))
-                    {
-                        string curItem = listBox2.SelectedItem.ToString();
-                        int index = listBox2.FindString(curItem);
-                        listBox2.SetSelected(index, true);
-                        object fileLocationobj = curItem;
-                        string fileLocation = fileLocationobj.ToString();
-                        // play mp3
-                        WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer
-                        {
-                            URL = fileLocation
-                        };
-
-                        wplayer.controls.play();
-                    }
-                    else
-                    {   string curItem = listBox2.SelectedItem.ToString();
-                        int index = listBox2.FindString(curItem);
-                        listBox2.SetSelected(index, true);
-                        object fileLocationobj = curItem;
-                        string fileLocation = fileLocationobj.ToString();
-                        // play wav
-                        SoundPlayer splayer = new SoundPlayer(soundLocation: fileLocation);
-                        splayer.Play();
-                    }
-                }
-
+            ProcessButtonClick(listBox2);
         }
-    }
+
         private void Button3_Click(object sender, EventArgs e)
         {
-            if (listBox3.Items.Count is 0)
-            { System.Windows.Forms.MessageBox.Show("No File Clown Shoes!"); }
-            else
-            {
-                if (listBox3.SelectedItems.Count is 0)
-                { System.Windows.Forms.MessageBox.Show("Stop being mean to me! select a file"); }
-                else
-                {
-
-                    if (listBox3.SelectedItem.ToString().EndsWith(".mp3"))
-                    {
-                        string curItem = listBox3.SelectedItem.ToString();
-                        int index = listBox3.FindString(curItem);
-                        listBox1.SetSelected(index, true);
-                        object fileLocationobj = curItem;
-                        string fileLocation = fileLocationobj.ToString();
-                        // play mp3
-                        WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer
-                        {
-                            URL = fileLocation
-                        };
-
-                        wplayer.controls.play();
-                    }
-                    else
-                    {   string curItem = listBox3.SelectedItem.ToString();
-                        int index = listBox3.FindString(curItem);
-                        listBox3.SetSelected(index, true);
-                        object fileLocationobj = curItem;
-                        string fileLocation = fileLocationobj.ToString();
-                        // play wav
-                        SoundPlayer splayer = new SoundPlayer(soundLocation: fileLocation);
-                        splayer.Play();
-                    }
-                }
-            }
+            ProcessButtonClick(listBox3);
         }
 
         private void Button4_Click(object sender, EventArgs e)
         {
-            if (listBox4.Items.Count is 0)
-            { System.Windows.Forms.MessageBox.Show("No File Clown Shoes!"); }
-            else
-            {
-                if (listBox4.SelectedItems.Count is 0)
-                { System.Windows.Forms.MessageBox.Show("Stop being mean to me! select a file"); }
-                else
-                {
-
-                    if (listBox4.SelectedItem.ToString().EndsWith(".mp3"))
-                    {
-                        string curItem = listBox4.SelectedItem.ToString();
-                        int index = listBox4.FindString(curItem);
-                        listBox4.SetSelected(index, true);
-                        object fileLocationobj = curItem;
-                        string fileLocation = fileLocationobj.ToString();
-                        // play mp3
-                        WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer
-                        {
-                            URL = fileLocation
-                        };
-
-                        wplayer.controls.play();
-                    }
-                    else
-                    {   string curItem = listBox4.SelectedItem.ToString();
-                        int index = listBox4.FindString(curItem);
-                        listBox4.SetSelected(index, true);
-                        object fileLocationobj = curItem;
-                        string fileLocation = fileLocationobj.ToString();
-                        // play wav
-                        SoundPlayer splayer = new SoundPlayer(soundLocation: fileLocation);
-                        splayer.Play();
-                    }
-                }
-
-            }
+            ProcessButtonClick(listBox4);
         }
 
         private void Button5_Click(object sender, EventArgs e)
         {
-            if (listBox5.Items.Count is 0)
-            { System.Windows.Forms.MessageBox.Show("No File Clown Shoes!"); }
-            else
-            {
-                if (listBox5.SelectedItems.Count is 0)
-                { System.Windows.Forms.MessageBox.Show("Stop being mean to me! select a file"); }
-                else
-                {
-
-                    if (listBox5.SelectedItem.ToString().EndsWith(".mp3"))
-                    {
-                        string curItem = listBox5.SelectedItem.ToString();
-                        int index = listBox5.FindString(curItem);
-                        listBox5.SetSelected(index, true);
-                        object fileLocationobj = curItem;
-                        string fileLocation = fileLocationobj.ToString();
-                        // play mp3
-                        WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer
-                        {
-                            URL = fileLocation
-                        };
-
-                        wplayer.controls.play();
-                    }
-                    else
-                    {
-                        string curItem = listBox5.SelectedItem.ToString();
-                        int index = listBox5.FindString(curItem);
-                        listBox5.SetSelected(index, true);
-                        object fileLocationobj = curItem;
-                        string fileLocation = fileLocationobj.ToString();
-                        // play wav
-                        SoundPlayer splayer = new SoundPlayer(soundLocation: fileLocation);
-                        splayer.Play();
-                    }
-                }
-
-            }
+            ProcessButtonClick(listBox5);
         }
 
         private void Button6_Click(object sender, EventArgs e)
         {
-            if (listBox6.Items.Count is 0)
-            { System.Windows.Forms.MessageBox.Show("No File Clown Shoes!"); }
-            else
-            {
-                if (listBox6.SelectedItems.Count is 0)
-                { System.Windows.Forms.MessageBox.Show("Stop being mean to me! select a file"); }
-                else
-                {
-                    if (listBox6.SelectedItem.ToString().EndsWith(".mp3"))
-                    {
-                        string curItem = listBox6.SelectedItem.ToString();
-                        int index = listBox6.FindString(curItem);
-                        listBox6.SetSelected(index, true);
-                        object fileLocationobj = curItem;
-                        string fileLocation = fileLocationobj.ToString();
-                        // play mp3
-                        WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer
-                        {
-                            URL = fileLocation
-                        };
-
-                        wplayer.controls.play();
-                    }
-                    else
-                    {
-
-                        
-                        string curItem = listBox6.SelectedItem.ToString();
-                        int index = listBox6.FindString(curItem);
-                        listBox6.SetSelected(index, true);
-                        object fileLocationobj = curItem;
-                        string fileLocation = fileLocationobj.ToString();
-                        // play wav
-                        SoundPlayer splayer = new SoundPlayer(soundLocation: fileLocation);
-                        splayer.Play();
-                    }
-                }
-
-            }
+            ProcessButtonClick(listBox6);
         }
-        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+
+        private void DragDropEvent(DragEventArgs e, ListBox listBox)
         {
- 
-
+            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            int i;
+            for (i = 0; i < s.Length; i++)
+                listBox.Items.Add(s[i]);
         }
-        private void ListBox1_DragEnter(object sender, System.Windows.Forms.DragEventArgs e)
+
+        private void DragEnterEvent(DragEventArgs e, ListBox listBox)
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
                 e.Effect = DragDropEffects.All;
             else
                 e.Effect = DragDropEffects.None;
         }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void ListBox1_DragEnter(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            DragEnterEvent(e, listBox1);
+        }
+
         public void ListBox1_DragDrop(object sender, System.Windows.Forms.DragEventArgs e)
         {
-            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
-            int i;
-            for (i = 0; i < s.Length;  i++)
-                listBox1.Items.Add(s[i]);
-    
-
-
-
+            DragDropEvent(e, listBox1);
         }
 
         private void ListBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
-
         }
+
         private void ListBox2_DragEnter(object sender, System.Windows.Forms.DragEventArgs e)
         {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
-                e.Effect = DragDropEffects.All;
-            else
-                e.Effect = DragDropEffects.None;
+            DragEnterEvent(e, listBox2);
         }
+
         public void ListBox2_DragDrop(object sender, System.Windows.Forms.DragEventArgs e)
         {
-            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
-            int i;
-            for (i = 0; i < s.Length; i++)
-                listBox2.Items.Add(s[i]);
+            DragDropEvent(e, listBox2);
         }
 
         private void ListBox3_SelectedIndexChanged(object sender, EventArgs e)
         {
-
         }
+
         private void ListBox3_DragEnter(object sender, System.Windows.Forms.DragEventArgs e)
         {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
-                e.Effect = DragDropEffects.All;
-            else
-                e.Effect = DragDropEffects.None;
+            DragEnterEvent(e, listBox3);
         }
+
         public void ListBox3_DragDrop(object sender, System.Windows.Forms.DragEventArgs e)
         {
-            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
-            int i;
-            for (i = 0; i < s.Length; i++)
-                listBox3.Items.Add(s[i]);
+            DragDropEvent(e, listBox3);
         }
 
         private void ListBox4_SelectedIndexChanged(object sender, EventArgs e)
         {
-
         }
+
         private void ListBox4_DragEnter(object sender, System.Windows.Forms.DragEventArgs e)
         {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
-                e.Effect = DragDropEffects.All;
-            else
-                e.Effect = DragDropEffects.None;
+            DragEnterEvent(e, listBox4);
         }
+
         public void ListBox4_DragDrop(object sender, System.Windows.Forms.DragEventArgs e)
         {
-            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
-            int i;
-            for (i = 0; i < s.Length; i++)
-                listBox4.Items.Add(s[i]);
+            DragDropEvent(e, listBox4);
         }
 
         private void ListBox5_SelectedIndexChanged(object sender, EventArgs e)
         {
-
         }
+
         private void ListBox5_DragEnter(object sender, System.Windows.Forms.DragEventArgs e)
         {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
-                e.Effect = DragDropEffects.All;
-            else
-                e.Effect = DragDropEffects.None;
+            DragEnterEvent(e, listBox5);
         }
+
         public void ListBox5_DragDrop(object sender, System.Windows.Forms.DragEventArgs e)
         {
-            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
-            int i;
-            for (i = 0; i < s.Length; i++)
-                listBox5.Items.Add(s[i]);
+            DragDropEvent(e, listBox5);
         }
 
         private void ListBox6_SelectedIndexChanged(object sender, EventArgs e)
         {
-
         }
+
         private void ListBox6_DragEnter(object sender, System.Windows.Forms.DragEventArgs e)
         {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
-                e.Effect = DragDropEffects.All;
-            else
-                e.Effect = DragDropEffects.None;
+            DragEnterEvent(e, listBox6);
         }
+
         public void ListBox6_DragDrop(object sender, System.Windows.Forms.DragEventArgs e)
         {
-            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
-            int i;
-            for (i = 0; i < s.Length; i++)
-                listBox6.Items.Add(s[i]);
+            DragDropEvent(e, listBox6);
         }
 
         private void Button7_Click(object sender, EventArgs e)
